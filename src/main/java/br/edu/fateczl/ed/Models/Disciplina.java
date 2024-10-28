aeb61a4790f0f2e359fbed58742ff72e9620bde7
triciopo <thiagoriciopo@gmail.com>
2024-10-28 20:43:46 -0300

    adicionando constructor vazio

--- src/main/java/br/edu/fateczl/ed/Models/Disciplina.java
+++ src/main/java/br/edu/fateczl/ed/Models/Disciplina.java
@@ -8,6 +8,8 @@ public class Disciplina {
     private String horasDiarias;
     private int codigoCurso;
 
+    public Disciplina() {}
+
     public Disciplina(int codigo, String nome, int diaSemana, String horario, String horasDiarias, int codigoCurso) {
         this.codigo = codigo;
         this.nome = nome;
